ef906862d89b54bec8626f10fa69c32c30b841fa
ashkash28 <aswinprakash18@gmail.com>
2020-12-07 09:01:03 -0800

    add redux-thunk for requestRobots action

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,31 +6,32 @@ import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css"
-import { setSearchField } from "../actions";
+import { setSearchField, requestRobots } from "../actions";
 
 const mapStateToProps = state => ({
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
 });
 
 function App(props) {
-    const { searchField, onSearchChange } = props;
-    const [robots, setRobots] = useState([]);
+    const { searchField, onSearchChange, robots, onRequestRobots, isPending  } = props;
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(data => setRobots(data))
+        onRequestRobots();
     }, []);
 
     const filteredRobots = robots
         .filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
 
-    return robots.length ? (
+    return !isPending ? (
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
             <button onClick={() => setCount(count + 1)}>Click Me!</button>
